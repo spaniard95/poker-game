e6ec9c0bafa7f504d60ff10c4102ecfdad6d0c35
spaniard95 <panagiotis.spanos95@gmail.com>
2020-10-28 21:03:30 +0200

    restructuring components

--- src/index.js
+++ src/index.js
@@ -1,22 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-
- function Card({rank ,suit}) {
+import '.components';
  
-     const rank2 = typeof rank === "string" ? rank.toUpperCase() : rank 
-     const className  = "card rank-" + rank + " " + suit
-     const suits = {spades: "2660", hearts: "2665", clubs: "2663", diamonds: "2666"}
-     const uni = String.fromCharCode("0x" + suits[suit]);
-     return ( 
-       <li>
-         <a className = {className}>
-              <span className = "rank">{rank2}</span>
-              <span className = "suit">{uni}</span>
-         </a>
-        </li>
-      );
-   }  
  
  class Player extends React.Component {
    
